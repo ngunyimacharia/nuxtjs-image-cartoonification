9ff9e39985fc702c669bc4339842c40abc867421
Kelvin Ngunyi <ngunyimacharia@gmail.com>
2021-11-11 06:17:35 +0200

    Cloudinary installed and cartoonification implemented

--- pages/index.vue
+++ pages/index.vue
@@ -21,25 +21,23 @@
       </div>
     </div>
 
-    <div
-      class="
-        bg-white
-        px-4
-        py-5
-        border
-        rounded-lg
-        border-gray-200
-        sm:px-6
-        w-2/3
-        mx-auto
-        text-center
-      "
-    >
+    <div v-if="cloudinaryImage" class="text-center">
+      <cld-image
+        :public-id="cloudinaryImage.public_id"
+        width="500"
+        height="400"
+        crop="pad"
+        background="black"
+        class="mx-auto"
+      >
+        <cld-transformation effect="cartoonify" />
+      </cld-image>
       <a
         v-if="cloudinaryImage"
         target="_blank"
         :href="imageUrl"
         class="
+          my-5
           inline-flex
           items-center
           px-4
@@ -59,7 +57,23 @@
       >
         Download
       </a>
-      <form v-else @submit.prevent="upload">
+    </div>
+
+    <div
+      class="
+        bg-white
+        px-4
+        py-5
+        border
+        rounded-lg
+        border-gray-200
+        sm:px-6
+        w-2/3
+        mx-auto
+        text-center
+      "
+    >
+      <form @submit.prevent="upload">
         <input
           class="
             my-5
@@ -114,7 +128,27 @@ export default {
     return {
       uploading: false,
       image: null,
-      cloudinaryImage: null,
+      cloudinaryImage: {
+        asset_id: "2bbe272f2615690bc5ae8f89beba2be9",
+        public_id: "nuxtjs-image-cartoonification/mjdqsl487qiqvnhouofa",
+        version: 1636603656,
+        version_id: "3c1980bd63536d878a957865ce912292",
+        signature: "0e0316918e36b564798352df20738a1393e54d46",
+        width: 4000,
+        height: 6000,
+        format: "jpg",
+        resource_type: "image",
+        created_at: "2021-11-11T04:07:36Z",
+        tags: [],
+        bytes: 2395779,
+        type: "upload",
+        etag: "47ae18f89e6d103071ed6c7b6b6a631c",
+        placeholder: false,
+        url: "http://res.cloudinary.com/hackit-africa/image/upload/v1636603656/nuxtjs-image-cartoonification/mjdqsl487qiqvnhouofa.jpg",
+        secure_url:
+          "https://res.cloudinary.com/hackit-africa/image/upload/v1636603656/nuxtjs-image-cartoonification/mjdqsl487qiqvnhouofa.jpg",
+        access_mode: "public",
+      },
     };
   },
   computed: {
